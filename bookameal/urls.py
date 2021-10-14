255503b91f7907fe7df1c91ede58bbe7ab9fabe6
Patrick <kimanthip@gmail.com>
2021-10-14 21:01:25 +0300

    make it better

--- bookameal/urls.py
+++ bookameal/urls.py
@@ -26,11 +26,6 @@ urlpatterns = [
     path('api/users/', include('users.api.urls')),
     path('api/menus/', include('menus.api.urls')),
     path('api/orders/', include('orders.api.urls')),
-    
-    
-    
-
-
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
